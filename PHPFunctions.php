6fd23dea63f05475ad496b77268619a392e7ce0b
Dave <Dave@DESKTOP-7GPI1I9>
2019-12-13 01:50:22 +0000

    Upload code

--- PHPFunctions.php
+++ PHPFunctions.php
@@ -1,42 +1,64 @@
-<?php
-    // Set variables for connecting to the SQL database
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "form_lib";
-            
-    // Connect to the SQL database
-    $sqlConnect = new mysqli($servername, $username, $password, $dbname);
-     
-    // Define variables ready for insertion into the database
-    $firstname = $sqlConnect->real_escape_string($_POST['first_name']);
-    $surname = $sqlConnect->real_escape_string($_POST['surname']);
-    $email = $sqlConnect->real_escape_string($_POST['email']);
-    $gender = $sqlConnect->real_escape_string($_POST['gender']);
-    $age = $sqlConnect->real_escape_string($_POST['age']);
-    $message = $sqlConnect->real_escape_string($_POST['message']);
-    $newsletter = $sqlConnect->real_escape_string($_POST['newsletter']);
-    $service = $sqlConnect->real_escape_string($_POST['service']);
-            
-    // Test connectiong to SQL server
-    if ($sqlConnect->connect_error) 
-    {
-        die("Connection failed: " . $sqlConnect->connect_error);
-    }
-            
-    // Insert the data into the SQL table
-    $sql = "INSERT INTO submissions (first_name, surname, email, gender, age, message, newsletter, service ) 
-            VALUES ('$firstname', '$surname', '$email', '$gender', '$age', '$message', '$newsletter', '$service')";
-    if ($sqlConnect->query($sql) === TRUE) 
-    {
-        echo "New record created successfully";
-    } 
-        else 
-        {
-            echo "Please try enter the above information again", '<br/>';
-            echo "There was an error loading the data in the database " . $sql . "<br>" . $sqlConnect->error;
-        }
-     
-    // Clos conection to the database
-    $sqlConnect->close();
-?>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <?php
+// Set variables for connecting to the SQL database
+            $servername = "localhost";
+            $username = "root";
+            $password = "root";
+            $dbname = "form_lib";
+
+// Connect to the SQL database
+            $sqlConnect = new mysqli($servername, $username, $password, $dbname);
+
+// Define variables ready for insertion into the database
+            $firstname = $sqlConnect->real_escape_string($_POST['first_name']);
+            $surname = $sqlConnect->real_escape_string($_POST['surname']);
+            $email = $sqlConnect->real_escape_string($_POST['email']);
+            $gender = $sqlConnect->real_escape_string($_POST['gender']);
+            $age = $sqlConnect->real_escape_string($_POST['age']);
+            $message = $sqlConnect->real_escape_string($_POST['message']);
+            $newsletter = $sqlConnect->real_escape_string($_POST['newsletter']);
+            $service = $sqlConnect->real_escape_string($_POST['service']);
+
+// Test connectiong to SQL server
+            if ($sqlConnect->connect_error) {
+                die("Connection failed: " . $sqlConnect->connect_error);
+            }
+
+// Insert the data into the SQL table
+            if (isset($_POST['submit'])) {
+                $sql = "INSERT INTO submissions (first_name, surname, email, gender, age, message, newsletter, service ) 
+VALUES ('$firstname', '$surname', '$email', '$gender', '$age', '$message', '$newsletter', '$service')";
+                echo "<b><u>Attempting to add the data into the SQL database</u></b>", '<br/>', '<br/>';
+                if ($sqlConnect->query($sql) === TRUE) {
+                    echo "New record has been added to the SQL database successfully", '<br/>', '<br/>';
+                } else {
+                    echo "The record has not been added to the SQL database", '<br/>';
+                    echo "Please try enter the above information again", '<br/>';
+                    echo "There was an error loading the data in the database " . $sql . "<br>" . $sqlConnect->error;
+                    echo '<br/>', '<br/>';
+                }
+            }
+
+// Close conection to the database
+            $sqlConnect->close();
+            ?>
+            <p><strong>Click the Button to return to the Main Page</strong></p>
+            <button type="button" onclick="redirectToMainPage()">Click Me!</button>
+        </div>
+
+        <script>
+            function redirectToMainPage() {
+                location.replace("http://localhost/PhpProject3/index.php")
+            }
+        </script>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -7,66 +7,99 @@ Task is to build a PHP, a form library from scratch.
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
         <title>PHP Interview Challenge</title>
     </head>
     <body>
-        <h1>PHP Interview Challenge</h1>
-        <strong>Please enter the below information and press submit</br></strong>
-        </br>
-        <table boarder=0" cellspacing="30">
-            <form action="" method="POST" id="PHPform" >
-                <input type="hidden" name="sid" value="1">
-                <tr>
-                    <td><p><label for="first_name">First name: </label></p></td>
-                    <td><p><input type="text" name="first_name" value="" required></p></td>
-                </tr>
-                <tr>
-                    <td><p><label for="surname">Last name: </label></p></td>
-                    <td><p><input type="text" name="surname" value="" required></p></td>
-                </tr>
-                <tr>
-                    <td><p><label for="email">Email address: </label></p></td>
-                    <td><p><input type="email" name="email" value="" required></p></td>
-                </tr>    
-                <tr>    
-                    <td><p><label for="gender">Gender: </label></p></td>
-                        <td><p><select name="gender">
-                            <option value="male">Male
-                            <option value="female">Female
-                            <option value="other">Other
-                        </select></p></td>
-                </tr>
-                <tr>
-                    <td><p><label for="age">Age: </label></p></td>
-                    <td><p><input type="text" name="age" value="" required></p></td>
-                </tr>
-                <tr>
-                    <td><p><label for="newsletter">Newsletter required: </label></p></td>
-                    <td><p><input type="checkbox" name="newsletter" value="1">Opt-In
-                    <input type="checkbox" name="newsletter" value="0" checked >Opt-Out</p></td>
-                </tr>
-                <tr>
-                    <td>
-                        <label for="service">Service required</label>
-                        <p><input type="radio" name="service" value="development" checked>Development<br/>
-                        <input type="radio" name="service" value="design" >Design<br>
-                        <input type="radio" name="service" value="optimisation" >Optimisation<br>
-                        <input type="radio" name="service" value="paid search" >Paid Search</p>
-                    </td>
-                </tr>
-            </form>
-            <tr>
-                <td><p><label for="message">Message: </label></p></td>
-                <td><p><textarea rows="4" cols="50" name="message" form="PHPform">Please enter message here...</textarea></p></td>
-            </tr>
+        <div class="jumbotron text-center">
+            <h1>PHP Interview Challenge</h1>
+            <strong>Please enter the below information and press submit</br></strong>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <form id="PHPform" action="PHPFunctions.php" method="POST" >
+                    <div class="col-sm-4">
+                        <table boarder="1">
+
+                            <tr>
+                                <td><p><label for="first_name">First name: </label></p></td>
+                                <td><p><input type="text" name="first_name" value="" required></p></td>
+                            </tr>
+                            <tr>
+                                <td><p><label for="surname">Last name: </label></p></td>
+                                <td><p><input type="text" name="surname" value="" required></p></td>
+                            </tr>
+                            <tr>
+                                <td><p><label for="email">Email address: </label></p></td>
+                                <td><p><input type="email" name="email" value="" required></p></td>
+                            </tr>    
+                            <tr>    
+                                <td><p><label for="gender">Gender: </label></p></td>
+                                <td><p><select name="gender">
+                                            <option value="male">Male
+                                            <option value="female">Female
+                                            <option value="other">Other
+                                        </select></p></td>
+                            </tr>
+                            <tr>
+                                <td><p><label for="age">Age: </label></p></td>
+                                <td><p><input type="text" name="age" value="" required></p></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div class="col-sm-4">
+                        <table boarder="1">
+                            <tr>
+                                <td><label for="newsletter">Newsletter required: </label>
+                                    <p><input type="checkbox" name="newsletter" value="1" id="newsletterIn">Opt-In<br/></p>
+                                    <p hidden><input type="checkbox" name="newsletter" value="0" id="newsletterOut" checked >Opt-Out</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <label for="service">Service required</label>
+                                    <p><input type="radio" name="service" value="development" checked>Development<br/>
+                                        <input type="radio" name="service" value="design" >Design<br/>
+                                        <input type="radio" name="service" value="optimisation" >Optimisation<br/>
+                                        <input type="radio" name="service" value="paid search" >Paid Search</p>
+                                </td>
+                            </tr>
+                    </div>    
+            </div>
         </table>
-        
-        <p><input type="submit" name="submit" value="Submit" form="PHPform" id="PHPformSubmit"></p>
+    </div>
+</form>
 
-        <?php require_once 'PHPFunctions.php' ?>
+<div class="col-sm-4">
+    <table boarder="1">
+        <tr>
+            <td><label for="message">Message: </label>
+                <p><textarea rows="4" cols="38" name="message" form="PHPform" required></textarea></p>
+            </td>
+        </tr>
+    </table>
+</div>
+</div>
+</div>
 
-        
-     </body>
+<div class="container">
+    <p><input type="submit" name="submit" value="Submit" form="PHPform" onclick="checkNewsletter()"></p>
+</div>
+
+<script>
+    function checkNewsletter()
+    {
+        if (document.getElementById("newsletterIn").checked === false)
+        {
+            document.getElementById("newsletterOut").checked = true;
+        }
+        document.getElementById("PHPform").submit();
+    }
+</script>
+
+</body>
 </html>
 
 
